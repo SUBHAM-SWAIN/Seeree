f6a27ba86a0d0bd86560d1430f5451986ce6f847
SUBHAM SWAIN <subhamswain277@gmail.com>
2025-07-09 16:34:34 +0530

    ADDed

--- Express/server.js
+++ Express/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 const port = 3000;
 const user = require("./user.json");
 app.use(express.urlencoded({ extended: false }));
@@ -7,6 +8,38 @@ app.use(express.json());
 
 app.get("/fetchData", (req, res) => {
   return res.json(user);
+});
+
+app.get("/singleUser/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const userData = user.find((user) => user.id == id);
+  console.log(userData);
+  return res.json(userData);
+});
+
+app.post("/createUser", (req, res) => {
+  const body = req.body;
+  if (
+    !body ||
+    !body.name ||
+    !body.email ||
+    !body.id ||
+    !body.age ||
+    !body.phone
+  ) {
+    {
+      return res.status(400).json({
+        message: "Please provide all the required fields",
+      });
+    }
+  } else {
+    user.push(body);
+    fs.writeFileSync("./user.json", JSON.stringify(user, null, 2));
+    return res.json({
+      message: "User created successfully",
+    });
+  }
 });
 
 app.listen(port, () => {
